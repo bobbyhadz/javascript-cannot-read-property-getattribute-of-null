2aadb89b25bf845940441cd1dc4ced5bf93aca1e
bobbyhadz <opmftw@gmail.com>
2024-03-02 20:07:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('bobbyhadz.com');
+
+const el = document.getElementById('box');
+console.log(el); // 👉️ div#box
+
+// ✅ Works
+const id = el.getAttribute('id');
+console.log(id); // 👉️ box
